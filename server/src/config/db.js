d2f85f8ced5f2e6f83669134d6b617db7c72b1b9
Inderjit Singh <sauwaliajatt@gmail.com>
2025-12-04 11:38:29 +0530

    Initial commit

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("MongoDB connected successfully")
     }catch(err){
-         console.error("MongoDB connection error:", err);
+        console.error("MongoDB connection error:", err);
         process.exit(1);
     }
 }
